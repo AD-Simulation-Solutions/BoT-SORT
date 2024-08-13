d3a45190cd50b5588a75d7495dee0ea8d1e9b017
Alexey Razgulyaev <alexey@welbelement.click>
2024-08-13 21:23:01 +0000

    package tracker

---
+++ setup_tracker.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    pass
+
+setup(
+    name="adss_botsort_fixes",
+    version="0.0.0.1",
+    author="Alexey Razgulyaev",
+    author_email="ar.work.issues@gmail.com",
+    description="Adss.Ai botsort fork with fixes",
+    license="property of adss ai",
+    packages=['tracker'],
+    classifiers=[]
+)
---
+++ tracker/__init__.py
@@ -0,0 +1,6 @@
+from .basetrack import *
+from .bot_sort import *
+from .gmc import *
+from .kalman_filter import *
+from .matching import *
+from .mc_bot_sort import *
